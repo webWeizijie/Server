e234e363d6f716b7636b348dabef3b1483ea3f04
“974960593@qq.com <974960593@qq.com>
2018-10-08 12:59:49 +0800

    合并

--- route/music/puppeteer.js
+++ route/music/puppeteer.js
@@ -3,10 +3,10 @@ const devices = require('puppeteer/DeviceDescriptors');
 const iPhone6 = devices['iPhone 6'];
 
 module.exports = async () => {
-    const browser = await puppeteer.launch({headless: true,timeout: 1000000,args: ['--disable-dev-shm-usage']});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.emulate(iPhone6);
-    await page.goto('https://i.y.qq.com/v8/playsong.html?songmid=004geL9D0EnH32&ADTAG=myqq&from=myqq&channel=10007100',{ waitUntil: 'networkidle2'});
+    await page.goto('https://i.y.qq.com/v8/playsong.html?songmid=004geL9D0EnH32&ADTAG=myqq&from=myqq&channel=10007100',{ waitUntil: 'networkidle2',timeout: 1000000});
     await page.waitFor(3000)
     const url = await page.evaluate(() => {
         let src = document.getElementById('h5audio_media').src.match(/guid=(.+?)&uin/)
