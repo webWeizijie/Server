565538097184475c4348d66820b07a38a159277c
weizijie <974960593@qq.com>
2018-10-08 10:39:24 +0800

    合并分之

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 const Koa = require('koa');
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
-const jobRoute = require('../route/job/index')
 const musicRoute = require('../route/music/index')
 const elemeRoute = require('../route/eleme')
-const resgisetSocket = require('../route/job/socket')
+//const jobRoute = require('../route/job/index')
+//const resgisetSocket = require('../route/job/socket')
 const path = require('path')
 const static = require('koa-static')
 const app = new Koa()
@@ -13,14 +13,14 @@ const router = new Router()
 
 app.use(bodyParser())
 app.use(static(path.join(__dirname, '../src/')));
-jobRoute(router, app) //job路由页面
+// jobRoute(router, app) //job路由页面
 musicRoute(router,app)//歌曲路由页面
 elemeRoute(router,app)
 
 app.use(router.routes()).use(router.allowedMethods())
 
 const server = require('http').createServer(app.callback());
-const io = require('socket.io')(server);
+// const io = require('socket.io')(server);
 // resgisetSocket(io)
 
 server.listen(80);
