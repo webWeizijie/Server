c73ff2428c9cb7b723c9d74658cfa9198919b93c
weizijie <974960593@qq.com>
2018-11-12 13:52:54 +0800

    a

--- route/music/puppeteer.js
+++ route/music/puppeteer.js
@@ -3,14 +3,20 @@ const devices = require('puppeteer/DeviceDescriptors');
 const iPhone6 = devices['iPhone 6'];
 
 module.exports = async () => {
-    const browser = await puppeteer.launch({headless: false,executablePath:'C:/chrome-win32/chrome.exe'});
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+    });
     const page = await browser.newPage();
     await page.emulate(iPhone6);
-    await page.goto('https://i.y.qq.com/v8/playsong.html?songmid=004geL9D0EnH32&ADTAG=myqq&from=myqq&channel=10007100',{ waitUntil: 'networkidle2',timeout: 1000000});
+    await page.goto('https://i.y.qq.com/v8/playsong.html?songmid=004geL9D0EnH32&ADTAG=myqq&from=myqq&channel=10007100', {
+        waitUntil: 'networkidle2',
+        timeout: 1000000
+    });
     await page.waitFor(3000)
     const url = await page.evaluate(() => {
         let src = document.getElementById('h5audio_media').src.match(/guid=(.+?)&uin/)
-        
+
         return src[1]
     })
     await browser.close();
